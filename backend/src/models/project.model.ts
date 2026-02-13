4215faa5c25565dcfd3f4c301cc1dd6a93376297
Amaresh Maharana <amareshcoder@gmail.com>
2026-02-13 20:34:29 +0530

    feat(models): add Project model with schema and interface

---
+++ backend/src/models/project.model.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface ProjectDocument extends Document {
+  name: string;
+  description: string | null;
+  emoji: string;
+  workspace: mongoose.Types.ObjectId;
+  createdBy: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const projectSchema = new Schema<ProjectDocument>(
+  {
+    name: { type: String, required: true, trim: true },
+    description: { type: String, required: false, default: null },
+    emoji: { type: String, required: true, default: "📁", trim: true },
+    workspace: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Workspace",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const ProjectModel = mongoose.model<ProjectDocument>(
+  "Project",
+  projectSchema,
+);
