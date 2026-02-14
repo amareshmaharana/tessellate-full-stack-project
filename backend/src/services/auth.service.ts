09579c8636efb80a795fb8e87059cef18cace7fe
Amaresh Maharana <amareshcoder@gmail.com>
2026-02-14 23:13:54 +0530

    feat(auth): create workspace and assign owner role on new user signup

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 import { UserModel } from "../models/user.model.js";
 import { AccountModel } from "../models/account.model.js";
+import { WorkspaceModel } from "../models/workspace.model.js";
+import { RoleModel } from "../models/roles-permission.model.js";
+import { Roles } from "../enums/role.enum.js";
+import { NotFoundException } from "../utils/appError.js";
+import { MemberModel } from "../models/member.model.js";
 
 export const loginOrCreateAccountService = async (data: {
   provider: string;
@@ -34,15 +39,45 @@ export const loginOrCreateAccountService = async (data: {
         providerId: providerId.toLowerCase(),
       });
       await account.save({ session });
+
+      //   create a new workspace for new user
+      const workspace = new WorkspaceModel({
+        name: `My Workspace`,
+        description: `Workspace for ${user.name}`,
+        owner: user._id,
+      });
+      await workspace.save({ session });
+
+      const ownerRole = await RoleModel.findOne({
+        name: Roles.OWNER,
+      });
+
+      if (!ownerRole) {
+        throw new NotFoundException("Owner role not found");
+      }
+
+      const member = new MemberModel({
+        userId: user._id,
+        workspace: workspace._id,
+        role: ownerRole._id,
+        joinedAt: new Date(),
+      });
+      await member.save({ session });
+
+      user.currentWorkspace = workspace._id as mongoose.Types.ObjectId;
+      await user.save({ session });
     }
 
     await session.commitTransaction();
     session.endSession();
+    console.log("Session ended...");
 
     return { user };
   } catch (error) {
     await session.abortTransaction();
     session.endSession();
     throw error;
+  } finally {
+    session.endSession();
   }
 };
