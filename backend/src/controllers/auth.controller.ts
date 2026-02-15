62ef938f2f823fb5388a7041b20bcd7f7d189f18
Amaresh Maharana <amareshcoder@gmail.com>
2026-02-15 14:09:19 +0530

    feat(auth): add google login callback controller

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+
+import { asyncHandler } from "../middlewares/asyncHandler.middleware.js";
+import { config } from "../config/app.config.js";
+
+export const googleLoginCallback = asyncHandler(
+  async (req: Request, res: Response) => {
+    const currentWorkspace = req.user?.currentWorkspace;
+
+    if (!currentWorkspace) {
+      return res.redirect(
+        `${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failure`,
+      );
+    }
+
+    return res.redirect(
+      `${config.FRONTEND_ORIGIN}/workspace/${currentWorkspace}`,
+    );
+  },
+);
