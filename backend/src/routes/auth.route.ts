48fefb26fa7e67485f586aeeb7e91ecc99c90eaa
Amaresh Maharana <amareshcoder@gmail.com>
2026-02-16 16:13:46 +0530

    feat(auth): add local authentication routes

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -2,10 +2,19 @@ import express from "express";
 import passport from "passport";
 
 import { config } from "../config/app.config.js";
-import { googleLoginCallback } from "../controllers/auth.controller.js";
+import {
+  googleLoginCallback,
+  loginUserController,
+  logoutUserController,
+  registerUserController,
+} from "../controllers/auth.controller.js";
 
 const failedUrl = `${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failure`;
 export const authRoutes = express.Router();
+
+authRoutes.post("/register", registerUserController);
+authRoutes.post("/login", loginUserController);
+authRoutes.post("/logout", logoutUserController);
 
 authRoutes.get(
   "/google",
