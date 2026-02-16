972ceece0cef60786088442533707c4d14147d27
Amaresh Maharana <amareshcoder@gmail.com>
2026-02-16 16:13:28 +0530

    feat(errorHandler): add ZodError validation handling

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -1,7 +1,22 @@
-import type { ErrorRequestHandler } from "express";
+import type { ErrorRequestHandler, Response } from "express";
 
 import { HTTP_STATUS } from "../config/http.config.js";
 import { AppError } from "../utils/appError.js";
+import { ZodError } from "zod";
+import { ErrorCodeEnum } from "../enums/error-code.enum.js";
+
+const formatZodError = (res: Response, error: ZodError) => {
+  const errors = error.issues.map((err) => ({
+    field: err.path.join("."),
+    message: err.message,
+  }));
+
+  return res.status(HTTP_STATUS.BAD_REQUEST).json({
+    message: "Validation error",
+    errors: errors,
+    errorCode: ErrorCodeEnum.VALIDATION_ERROR,
+  });
+};
 
 export const errorHandler: ErrorRequestHandler = (
   error,
@@ -16,6 +31,10 @@ export const errorHandler: ErrorRequestHandler = (
       message: "Invalid JSON format. Please check your request payload.",
       error: error.message || "Unknown error occurred",
     });
+  }
+
+  if (error instanceof ZodError) {
+    return formatZodError(res, error);
   }
 
   if (error instanceof AppError) {
